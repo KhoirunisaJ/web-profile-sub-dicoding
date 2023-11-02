e1c00beef61612a619c04315bfd94995895325bd
Khoirunisa Jannatuzzahra <105572998+KhoirunisaJ@users.noreply.github.com>
2023-11-02 23:46:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Web Profile</title>
+        <link rel="stylesheet" href="asset/styles/style.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+        <!--Header-->
+        <header class="head">
+            <h1> MY PROFILE</h1>
+            <nav>
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#AboutMe">AboutMe</a></li>
+                    <li><a href="#Contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!--main-->
+        <main>
+            <article class="home" id="Home">
+                <img src="asset/">
+                <p>Khoirunisa Jannatuzzahra</p>
+                <p>Cloud Enginer</p>
+            </article>
+            <article class="aboutme" id="AboutMe">
+                    <div class="Biodata">
+                        <table>
+                            <H2>My Biodata</H2>
+                            <tr>
+                                <th>Nama</th>
+                                <td>: </td>
+                                <td>Khoirunisa Jannatuzzahra</td>
+                            </tr>
+                            <tr>
+                                <th>Path</th>
+                                <td>: </td>
+                                <td>Cloud Computing 44</td>
+                            </tr>
+                            <tr>
+                                <th>Email</th>
+                                <td>: </td>
+                                <td>kjzahra@gmailcom</td>
+                            </tr>
+                            <tr>
+                                <th>Prodi</th>
+                                <td>: </td>
+                                <td>Sistem Informasi</td>
+                            </tr>
+                            <tr>
+                                <th>Asal</th>
+                                <td>: </td>
+                                <td>UPN Veteran Jawa Timur</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="MyStory">
+                        <h2>My Story</h2>
+                        <p>
+                            My name is Khoirunisa Jannatuzzahra, now im a part of Cloud Computing Cohort
+                            at Bangkit Academy 2023 with Google, GoTo, Traveloka. Iam student at 
+                            UPN Veteran Jawa Timur on Information System major.Now i stand on semester 5.
+                        </p>
+                        <p> 
+                            I'm interested in learning about cloud computing. I have a big desire to be able
+                            to develop a career in this field. But I also have a hidden dream to become a "product manager". 
+                            This is confusing for me. I will try my best to pursue those dreams.
+                            Now I'm enjoying learning on the Bangkit academy using dicoding platforms. 
+                            I'm struggling to pass this program and get the best grades. 
+                            I hope it will be easier for me to pursue my dreams.
+                        </p>
+                    </div>
+            </article>
+            <aside>
+                <article>
+                    <div class="contact" id="Contact">
+                        <h2>Contact Me</h2>
+                        <p>
+                            Im happy to connect with you. Find me:
+                        </p>
+                        <ul>
+                            <li><a href="https://youtube.com/@khoirunisaj7295?si=i-gTN_ApOPn7Gh-R"><i class="fab fa-youtube"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/khoirunisajannatuzzahra/"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/khoirunisajannatuzzahra/"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/khoirunisajannatuzzahra/"><i class="fab fa-facebook"></i></a></li>
+                        </ul> 
+                    </div>
+                </article>
+            </aside>
+        </main>
+        <!--Footer-->
+        <footer>
+            <p>Web Profile &#169; 2023, Submission Dicoding</p>
+        </footer>
+    </body>
+
+</html>
